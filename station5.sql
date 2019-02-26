46b525c41ccb1b28aab7fa01d1c0659821797be7
Alex <0alexgutierrez@gmail.com>
2019-02-26 15:59:46 -0700

    added next sql problem

--- station5.sql
+++ station5.sql
@@ -23,3 +23,10 @@ FROM STATION )
 ORDER BY CITY
 LIMIT 1;
 
+/*
+https://www.hackerrank.com/challenges/weather-observation-station-6/problem?h_r=next-challenge&h_v=zen
+*/
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY REGEXP "^[aeiou].*"
