6357dcd273dfd874f136a3df613e0878cb40e8c2
Alex <0alexgutierrez@gmail.com>
2019-02-27 14:25:37 -0700

    added more select queries

--- station5.sql
+++ station5.sql
@@ -24,9 +24,43 @@ ORDER BY CITY
 LIMIT 1;
 
 /*
+Starting with vowel
 https://www.hackerrank.com/challenges/weather-observation-station-6/problem?h_r=next-challenge&h_v=zen
 */
 
 SELECT DISTINCT CITY
 FROM STATION
 WHERE CITY REGEXP "^[aeiou].*"
+
+/*
+Ending with vowel
+https://www.hackerrank.com/challenges/weather-observation-station-7/problem
+*/
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY REGEXP "[aeiou]$"
+
+/*
+Starting or ending in vowel
+*/
+SELECT CITY
+FROM STATION
+WHERE CITY REGEXP '^[aeiou].*[aeiou]$'
+
+/*
+Does not start with vowel
+*/
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY NOT REGEXP "^[aeiou].*"
+
+/*
+Query the Name of any student in STUDENTS who scored higher than 75 Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+https://www.hackerrank.com/challenges/more-than-75-marks/problem
+*/
+SELECT NAME
+FROM STUDENTS
+WHERE MARKS > 75
+ORDER BY
+  (SELECT RIGHT(NAME, 3)) ASC, ID ASC
